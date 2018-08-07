47acedfe5e21630e7917884c111d170d145b7574
Keren Cerezo <kerencerezo@Kerens-MacBook.local>
2018-08-06 23:09:01 -0400

    updated js

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 $(document).ready(function () {
-    var celeb = ["Beyonce", "Drake", "Lady Antebellum"];
-  
-    $("button").on("click", function () {
-  
-      var celebrities = $(this).attr("data-celeb");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=KvEqWrylBme2rVug5Kv6Zpvg2fY4mr2l&q="+celebrities
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (response) {
-        console.log(response);
-  
-        var results = response.data;
-  
-        for (var i = 0; i < results.length; i++) {
-  
-          var celebDiv = $("<div>");
-  
-          var celebImage = $("<img>")
+  var celeb = ["Beyonce", "Drake", "Lady Antebellum"];
 
-          var p = $("<p>").text("Rating: " + results[i].rating);
-  
-          celebImage.attr("src", results[i].images.fixed_height.url);
-  
-          celebDiv.append(p);
-          celebDiv.append(celebImage);
-  
-          $("#gifs-appear-here").prepend(celebDiv);
-        }
-      });
+  $("button").on("click", function () {
+
+    var celebrities = $(this).attr("data-celeb");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=KvEqWrylBme2rVug5Kv6Zpvg2fY4mr2l&q="+celebrities
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+
+        var celebDiv = $("<div>");
+
+        var celebImage = $("<img>")
+
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        celebImage.attr("src", results[i].images.fixed_height.url);
+
+        celebDiv.append(p);
+        celebDiv.append(celebImage);
+
+        $("#gifs-appear-here").prepend(celebDiv);
+      }
     });
   });
-  
+});
